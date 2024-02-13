d3989ade644f5558b31439d247a17cecf725b9a8
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-02-13 14:53:58 -0500

    Clean up JS

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const http = require('http');
-// const url = require('url');
 const fs = require('fs/promises');
 
-// create a server object
+// create server object
 http
   .createServer(async (req, res) => {
-    console.log(`url: ${req.url}`);
-    // const q = url.parse(req.url, true);
-    // console.log(`q: ${q.pathname}`);
     const fileName = req.url === '/' ? `./index.html` : `.${req.url}.html`;
-    console.log(`file name: ${fileName}`);
-
     let data;
+
     try {
       data = await fs.readFile(fileName);
       res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -22,15 +17,4 @@ http
     res.write(data); // write response to client
     return res.end(); // end response
   })
-  .listen(8080); // the server object listens on port 8080
-
-// fs.readFile(fileName, async (err, data) => {
-//   if (err) {
-//     res.writeHead(404, { 'Content-Type': 'text/html' });
-//     res.write('404');
-//     return res.end();
-//   }
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write(data); // write a response to the client
-//   return res.end(); // end the response
-// });
+  .listen(8080); // server object listens on port 8080
